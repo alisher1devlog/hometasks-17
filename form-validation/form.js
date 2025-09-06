b8830e18e2e99393c15a13c9189a3ec743ce34be
Alisher <alisheryondoshaliyev77@gmail.com>
2025-09-06 12:44:34 +0500

    some update

--- form-validation/form.js
+++ form-validation/form.js
@@ -16,3 +16,45 @@ nameEl.addEventListener("input", (e)=>{
         errorDiv.style.display = "none";
     }
 });
+
+emailEl.addEventListener("input", (e)=>{
+    const email = emailEl.value;
+    const errorDiv = document.querySelector("#emailErrorl");
+
+    if (!validateEmail(email)) {
+        emailEl.classList.add("error");
+        errorDiv.textContent = "Emailda kamida bitta @, bitta . orasida harf bo'lishi lozim!.";
+        errorDiv.style.display = "block";
+    }else{
+        emailEl.classList.remove("error");
+        errorDiv.style.display = "none";
+    }
+});
+
+passwordEl.addEventListener("input", (e) =>{
+    const password = passwordEl.value;
+    const errorDiv = document.querySelector("#passwordError");
+
+    if (!validatePassword(password)) {
+        passwordEl.classList.add("error");
+        errorDiv.textContent = "password kamida bitta 8 ta belgidan iborat bo'lishi lozim!.";
+        errorDiv.style.display = "block";
+    }else{
+        passwordEl.classList.remove("error");
+        errorDiv.style.display = "none"
+    }
+})
+
+retypePasswordEl.addEventListener("input", (e) =>{
+    const password = retypePasswordEl.value;
+    const errorDiv = document.querySelector("#retypePasswordError");
+
+    if (!validatePassword(password)) {
+        retypePasswordEl.classList.add("error");
+        errorDiv.textContent = "Retype Password kamida bitta 8 ta belgidan iborat bo'lishi lozim!.";
+        errorDiv.style.display = "block";
+    }else{
+        retypePasswordEl.classList.remove("error");
+        errorDiv.style.display = "none"
+    }
+})
