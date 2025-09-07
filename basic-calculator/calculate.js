aaba0afe898d11702199ebb9ef828ff7572fe59f
Alisher <alisheryondoshaliyev77@gmail.com>
2025-09-07 13:22:06 +0500

    finished

--- basic-calculator/calculate.js
+++ basic-calculator/calculate.js
@@ -1,2 +1,31 @@
 const number1El = document.querySelector("#number1");
 const number2El = document.querySelector("#number2");
+const finalResultEl = document.querySelector("#finalResult");
+const operatorEl = document.querySelector("#operator");
+const calculateEl = document.querySelector("#calculate");
+const resetEl = document.querySelector("#reset");
+
+calculateEl.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const operator =operatorEl.value;
+    const number1 = number1El.valueAsNumber;
+    const number2 = number2El.valueAsNumber;
+    if (operator === "add" ) {
+        let calculate = number1 + number2;
+        finalResultEl.textContent = calculate;
+    }else if (operator === "min" ) {
+        let calculate = number1 - number2;
+        finalResultEl.textContent = calculate;
+    }else if (operator === "mul" ) {
+        let calculate = number1 * number2;
+        finalResultEl.textContent = calculate;
+    }else if (operator === "div" ) {
+        let calculate = number1 / number2;
+        finalResultEl.textContent = calculate;
+    } 
+});
+
+resetEl.addEventListener("click", (e)=>{
+    finalResultEl.textContent = "";
+});
+
